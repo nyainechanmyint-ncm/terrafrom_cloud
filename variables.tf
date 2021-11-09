7f6785ee7b54c85fdb7508b5d4cf1c7a021b4593
nyainechanmyint-ncm <nyainechanmyint@outlook.com>
2021-11-09 15:44:52 +0630

    changes in variables

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "ami" {
+type = string
+}
+
+variable "instance_type" {
+type = string
+}
+
+variable "key_name" {
+type = string
+}
+
+variable "monitoring" {
+type = bool
+}
+
+variable "vpc_security_group_ids" {
+type = list(string)
+}
+
+variable "subnet_id" {
+type = string
+}
