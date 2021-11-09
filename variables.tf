70d10fac41600585a6bd7bc6ac3b68549bd903d3
nyainechanmyint-ncm <nyainechanmyint@outlook.com>
2021-11-09 15:52:51 +0630

    Correct in variable

--- variables.tf
+++ variables.tf
@@ -1,23 +1,27 @@
-variable "ami" {
-type = string
-}
+#variable "ami" {
+#type = string
+#}
 
 variable "instance_type" {
 type = string
 }
 
-variable "key_name" {
-type = string
-}
+#variable "key_name" {
+#type = string
+#}
 
-variable "monitoring" {
-type = bool
-}
+#variable "monitoring" {
+#type = bool
+#}
 
-variable "vpc_security_group_ids" {
-type = list(string)
-}
+#variable "vpc_security_group_ids" {
+#type = list(string)
+#}
 
-variable "subnet_id" {
+#variable "subnet_id" {
+#type = string
+#}
+
+variable "instance_type" {
 type = string
 }
