652cb61b135ab6508c6371b35def30d4b8e2e964
nyainemyint@gmail.com <nyainemyint@gmail.com>
2022-01-23 20:14:21 +0630

    latest changes

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "single-instance-1"
+  name = "single-instance"
 
   ami                    = "ami-0133407e358cc1af0"
   instance_type          = "t2.micro"
