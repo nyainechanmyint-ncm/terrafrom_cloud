5707ed59446156f58336ce1a710192ae93af9c7e
nyainemyint@gmail.com <nyainemyint@gmail.com>
2022-01-23 20:31:45 +0630

    latest changes output

--- main.tf
+++ main.tf
@@ -22,4 +22,9 @@ module "ec2_instance" {
     Terraform   = "true"
     Environment = "dev"
   }
+output "instance_ip_addr" 
+{  
+value       = aws_instance.server.private_ip  
+description = "The private IP address of the main server instance."
+}
 }
